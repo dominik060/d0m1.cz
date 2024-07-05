b4ad86fb0c750633d7cdc49ee24c7a5cd965f4f7
Dominik <dominikbartuska@gmail.com>
2024-07-05 19:52:39 +0200

    Delete useless code

--- src/components/HrFullWidth.tsx
+++ src/components/HrFullWidth.tsx
@@ -1,5 +1,5 @@
 export default function HrFullWidth() {
-    return (
-        <hr className="80rem:-translate-x-[calc((100vw-80rem)/2)] h-[2px] w-screen transform border-0 bg-current" />
-    );
+  return (
+    <hr className="h-[2px] w-screen transform border-0 bg-current xl:-translate-x-[calc((100vw-80rem)/2)]" />
+  );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,33 +1,29 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-    content: [
-        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    ],
-    theme: {
-        fontSize: {
-            h1: "48px",
-            h2: "30px",
-            h3: "24px",
-            h4: "20px",
-            p: "16px",
-        },
-
-        extend: {
-            colors: {
-                black: "#09090B",
-                white: "#FAFAFA",
-            },
-            fontFamily: {
-                sans: ["var(--font-geist-sans)"],
-            },
-            screens: {
-                "80rem": "80rem",
-            },
-        },
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    fontSize: {
+      h1: "48px",
+      h2: "30px",
+      h3: "24px",
+      h4: "20px",
+      p: "16px",
     },
-    plugins: [],
+    extend: {
+      colors: {
+        black: "#09090B",
+        white: "#FAFAFA",
+      },
+      fontFamily: {
+        sans: ["var(--font-geist-sans)"],
+      },
+    },
+  },
+  plugins: [],
 };
 export default config;
