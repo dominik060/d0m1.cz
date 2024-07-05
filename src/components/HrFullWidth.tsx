7d4286ce7b94951052ee25c4ada80aed67c9e7de
Dominik <dominikbartuska@gmail.com>
2024-07-05 15:48:08 +0200

    reformat

--- src/components/HrFullWidth.tsx
+++ src/components/HrFullWidth.tsx
@@ -1,10 +1,5 @@
 export default function HrFullWidth() {
     return (
-        <hr
-            className={`
-              h-[2px] w-screen -translate-x-[calc((100vw-80rem)/2)] transform
-              border-0 bg-current
-            `}
-        />
+        <hr className="h-[2px] w-screen -translate-x-[calc((100vw-80rem)/2)] transform border-0 bg-current" />
     );
 }
