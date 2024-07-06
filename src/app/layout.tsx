cb0471bd470d82bd5233dd31a15d8ffe64882348
Dominik <dominikbartuska@gmail.com>
2024-07-06 14:10:56 +0200

    add theme color to metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,10 +3,12 @@ import "./globals.css";
 import { GeistSans } from "geist/font/sans";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Providers } from "./providers";
+import type { Viewport } from "next";
 
 export const metadata: Metadata = {
   title: "Dominik Bartuška",
   description: "Personal web of Dominik Bartuška",
+  creator: "Dominik Bartuška",
   icons: [
     {
       rel: "icon",
@@ -20,6 +22,13 @@ export const metadata: Metadata = {
       url: "/favicon-dark.ico",
       media: "(prefers-color-scheme: dark)",
     },
+  ],
+};
+
+export const viewport: Viewport = {
+  themeColor: [
+    { media: "(prefers-color-scheme: light)", color: "#FAFAFA" },
+    { media: "(prefers-color-scheme: dark)", color: "#09090B" },
   ],
 };
 
