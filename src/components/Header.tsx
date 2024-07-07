e35b8218292f86f07be7eb22f3a1a0b120adad7e
Dominik <dominikbartuska@gmail.com>
2024-07-07 15:28:38 +0200

    create hero section

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,18 @@
+import HrFullWidth from "./HrFullWidth";
 import NavBar from "./navbar/NavBar";
 
 type Props = {
   className?: string;
+  children?: React.ReactNode;
+  [prop: string]: any;
 };
 
-export default function Header({ className }: Props) {
+export default function Header({ className, children, ...props }: Props) {
   return (
-    <header>
+    <header {...props} className={className}>
       <NavBar />
+      <HrFullWidth />
+      {children}
     </header>
   );
 }
