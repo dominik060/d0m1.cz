c8297b0ef8241081131ffc02c2cb8044e578781f
Dominik <dominikbartuska@gmail.com>
2024-07-07 17:06:58 +0200

    create hero component and make it responsible

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,13 @@
 import Header from "@/components/Header";
+import Hero from "@/components/Hero";
 import HrFullWidth from "@/components/HrFullWidth";
 
 export default function Home() {
   return (
     <main className="col flex min-h-screen w-screen items-center overflow-hidden bg-white text-left dark:bg-black">
       <div className="mx-auto flex min-h-screen w-full max-w-[80rem] flex-col gap-0 border-x-2 border-current text-current">
-        <Header className="h-screen">
-          <div className="flex h-[80%] flex-col items-center justify-center text-center">
-            <h1 className="font-hero bg-gradient-apple bg-clip-text text-h1 font-black text-transparent">
-              Forming Tomorrow's
-              <br />
-              Digital Landscape.
-            </h1>
-          </div>
+        <Header className="h-[90vh]">
+          <Hero />
         </Header>
 
         <HrFullWidth />
---
+++ src/components/Hero.tsx
@@ -0,0 +1,11 @@
+export default function Hero() {
+  return (
+    <div className="flex h-[80%] flex-col items-center justify-center text-center">
+      <h1 className="bg-gradient-apple bg-clip-text text-h1 font-black text-transparent sm:text-h1 md:text-[60px] lg:text-hero">
+        Forming Tomorrow's
+        <br />
+        Digital Landscape.
+      </h1>
+    </div>
+  );
+}
