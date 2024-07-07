c01fc55482e40735f358551590149532eaf56d22
Dominik <dominikbartuska@gmail.com>
2024-07-07 17:06:31 +0200

    temporary solution for responsive design for mobile sreen

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -14,7 +14,7 @@ export default function NavBar({ className, ...props }: Props) {
       <Link href="/" title="Přejít na domovskou stránku" className="">
         <Logo className="w-16" aria-hidden="true" />
       </Link>
-      <div className="flex flex-wrap items-center gap-8">
+      <div className="flex flex-wrap items-center justify-end gap-8">
         <NavLinks
           className="flex flex-nowrap gap-8 text-h4"
           navLinkList={[
