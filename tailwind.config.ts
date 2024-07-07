28fa97fd3366331ad32c838780235b6b8e9f1945
Dominik <dominikbartuska@gmail.com>
2024-07-07 15:28:10 +0200

    add background gradients

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,14 +13,28 @@ const config: Config = {
       h3: "24px",
       h4: "20px",
       p: "16px",
+      hero: "96px",
     },
     extend: {
       colors: {
         black: "#09090B",
         white: "#FAFAFA",
+        "gradient-apple":
+          "linear-gradient(43deg, #4158D0 0%, #C850C0 46%, #FFCC70 100%)",
       },
       fontFamily: {
         sans: ["var(--font-geist-sans)"],
+      },
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "gradient-apple":
+          "linear-gradient(43deg, #4158D0 0%, #C850C0 46%, #FFCC70 100%)",
+        "gradient-hero-dark":
+          "linear-gradient(90deg, #FAFAFA 0%, #EBEBEB 26%, #3AB6FF 79%, #226BB8 100%)",
+        "gradient-hero-light":
+          "linear-gradient(43deg, #09090B 0%, #202026 26%, #3AB6FF 79%, #226BB8 100%)",
       },
     },
   },
