35b562ede3b03c4e061a6ca419204076c31a943d
Dominik Bartuška <dominikbartuska@gmail.com>
2024-07-27 16:45:43 +0200

    remove dummy text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,33 +10,8 @@ export default function Home() {
         <Header className="h-[90vh]">
           <Hero />
         </Header>
-
         <HrFullWidth />
-
         <AboutMe />
-
-        <HrFullWidth />
-
-        <div className="max-w-full overflow-hidden px-4">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Id ipsum
-          atque quod rem facere placeat, aliquam pariatur quisquam iure! Itaque
-          accusamus recusandae, quasi eligendi repellat animi voluptatibus
-          repellendus voluptatum doloribus? Adipisci nemo, ratione eos provident
-          quia, voluptatibus odio sequi excepturi minima laudantium quos? Ex
-          itaque esse sed, incidunt amet quod nihil repudiandae dolores
-          quibusdam accusantium vero consectetur quisquam qui labore. Quo
-          veritatis autem, non nam, deleniti eum et voluptas culpa quaerat ex,
-          id aliquam quia ducimus dicta. Inventore corrupti nesciunt
-          exercitationem molestiae, fuga accusamus ipsa odit placeat quos, natus
-          culpa? Eligendi, odio praesentium ratione eius laudantium vitae
-          sapiente aspernatur voluptate aperiam ex recusandae voluptatum, est
-          similique. Ducimus, doloremque facere temporibus praesentium
-          accusantium a, provident similique repudiandae excepturi mollitia
-          perspiciatis dignissimos! Ratione ipsa, sed aperiam atque quo magnam
-          voluptas, nisi, hic saepe consectetur quae perspiciatis placeat
-          nostrum. Corrupti reprehenderit voluptas numquam placeat dolores sit
-          tempora, excepturi quos tenetur accusantium, sunt vel!
-        </div>
       </div>
     </main>
   );
