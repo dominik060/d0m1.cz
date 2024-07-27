6392bccf6a64abeeb7a133f13091994f74e7a93e
Dominik Bartuška <dominikbartuska@gmail.com>
2024-07-27 12:31:24 +0200

    change hero font size

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,8 @@
 export default function Hero() {
   return (
     <div className="flex h-[80%] flex-col items-center justify-center text-center">
-      <h1 className="bg-gradient-apple bg-clip-text text-h1 font-black text-transparent sm:text-h1 md:text-[60px] lg:text-hero">
+      <h1 className="text-3xl md:text-6xl bg-gradient-apple bg-clip-text font-black text-transparent">
+        {/* text-h1 sm:text-h1 md:text-[60px] lg:text-hero */}
         Forming Tomorrow's
         <br />
         Digital Landscape.
