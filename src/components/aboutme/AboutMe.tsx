62e1b024b85c352babdad4f387940ffaa9a0dfa1
Dominik Bartuška <dominikbartuska@gmail.com>
2024-07-27 15:22:33 +0200

    create about me folder

--- src/components/aboutme/AboutMe.tsx
+++ src/components/aboutme/AboutMe.tsx
@@ -7,7 +7,7 @@ export default function AboutMe({ className }: Props) {
     <div className="my-24 flex flex-col justify-center">
       <h2 className="text-2xl md:text-4xl text-center">
         Hello, I am{" "}
-        <span className="inline-block bg-gradient-apple bg-clip-text font-bold text-transparent duration-300 hover:-rotate-6">
+        <span className="inline-block bg-gradient-apple bg-clip-text font-bold text-transparent underline duration-300 hover:-rotate-6">
           Dominik Bartuška
         </span>
         !
