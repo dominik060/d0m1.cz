601b98e587d516bb7923b4b94a0203ec884aa19b
Dominik Bartuška <dominikbartuska@gmail.com>
2024-07-27 17:26:51 +0200

    make hero title bigger on smaller devices

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,7 @@
 export default function Hero() {
   return (
     <div className="flex h-[80%] flex-col items-center justify-center text-center">
-      <h1 className="text-3xl md:text-6xl bg-gradient-hero-light bg-clip-text font-black text-transparent dark:bg-gradient-hero-dark">
+      <h1 className="text-4xl md:text-6xl bg-gradient-hero-light bg-clip-text font-black text-transparent dark:bg-gradient-hero-dark">
         {/* text-h1 sm:text-h1 md:text-[60px] lg:text-hero */}
         Forming Tomorrow's
         <br />
