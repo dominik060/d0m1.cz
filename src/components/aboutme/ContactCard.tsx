e48344c89bfad6184ce40e0e5120cfe5c4fb2d03
Dominik Bartuška <dominikbartuska@gmail.com>
2024-07-27 16:34:34 +0200

    add Link

--- src/components/aboutme/ContactCard.tsx
+++ src/components/aboutme/ContactCard.tsx
@@ -1,10 +1,13 @@
+import Link from "next/link";
 import Card from "../Card";
+import { Url } from "next/dist/shared/lib/router/router";
 
 type Props = {
   className?: string;
   Icon: React.ReactElement;
   title: string;
   description: string;
+  link: Url;
 };
 
 export default function ContactCard({
@@ -12,17 +15,20 @@ export default function ContactCard({
   Icon,
   title,
   description,
+  link,
 }: Props) {
   return (
-    <Card className="group relative flex min-w-52 flex-col items-center gap-8 pb-16 pt-8">
-      <div className="bg- z-50 rounded-full border border-black border-opacity-40 bg-white p-4 dark:border-white dark:border-opacity-40 dark:bg-black">
-        <Icon.type />
-      </div>
-      <span className="via-current/50 absolute h-3/4 w-px bg-gradient-to-b from-current to-transparent"></span>
-      <div className="flex flex-col gap-3 text-center">
-        <h3 className="font-bold">{title}</h3>
-        <h4 className="text-white/50">{description}</h4>
-      </div>
-    </Card>
+    <Link href={link}>
+      <Card className="group relative flex min-w-52 flex-col items-center gap-8 pb-16 pt-8">
+        <div className="bg- z-50 rounded-full border border-black border-opacity-40 bg-white p-4 dark:border-white dark:border-opacity-40 dark:bg-black">
+          <Icon.type />
+        </div>
+        <span className="via-current/50 absolute h-3/4 w-px bg-gradient-to-b from-current to-transparent"></span>
+        <div className="flex flex-col gap-3 text-center">
+          <h3 className="font-bold">{title}</h3>
+          <h4 className="text-white/50">{description}</h4>
+        </div>
+      </Card>
+    </Link>
   );
 }
