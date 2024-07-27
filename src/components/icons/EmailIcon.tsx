17d1ff0befd93b425a8108d1a5d1dc24cf786b6c
Dominik Bartuška <dominikbartuska@gmail.com>
2024-07-27 15:52:07 +0200

    add email icon as component

---
+++ src/components/icons/EmailIcon.tsx
@@ -0,0 +1,26 @@
+export default function EmailIcon() {
+  return (
+    <svg
+      width="32"
+      height="32"
+      viewBox="0 0 32 32"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M26.667 5.3335H5.33366C3.8609 5.3335 2.66699 6.5274 2.66699 8.00016V24.0002C2.66699 25.4729 3.8609 26.6668 5.33366 26.6668H26.667C28.1397 26.6668 29.3337 25.4729 29.3337 24.0002V8.00016C29.3337 6.5274 28.1397 5.3335 26.667 5.3335Z"
+        stroke="#FAFAFA"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M29.3337 9.3335L17.3737 16.9335C16.962 17.1914 16.4861 17.3282 16.0003 17.3282C15.5146 17.3282 15.0386 17.1914 14.627 16.9335L2.66699 9.3335"
+        stroke="#FAFAFA"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+}
