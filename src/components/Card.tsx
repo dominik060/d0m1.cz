cd9fea185b2179d4e7a6abd060d1c05f1000b37b
Dominik Bartuška <dominikbartuska@gmail.com>
2024-07-27 15:37:45 +0200

    stylize card component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -3,5 +3,15 @@ type Props = {
   children: React.ReactNode;
 };
 export default function Card({ className, children }: Props) {
-  return <div className="">{children}</div>;
+  className = " " + className;
+  return (
+    <div
+      className={
+        "rounded-lg border-2 border-black border-opacity-60 p-4 dark:border-white dark:border-opacity-60" +
+        className
+      }
+    >
+      {children}
+    </div>
+  );
 }
