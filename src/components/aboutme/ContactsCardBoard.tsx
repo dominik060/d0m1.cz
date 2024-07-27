75cc1df9f96eafab21c6da4ccfcc9a1512d9471e
Dominik Bartuška <dominikbartuska@gmail.com>
2024-07-27 17:10:09 +0200

    create contact list

--- src/components/aboutme/ContactsCardBoard.tsx
+++ src/components/aboutme/ContactsCardBoard.tsx
@@ -2,21 +2,33 @@ import EmailIcon from "../icons/EmailIcon";
 import GitHubIcon from "../icons/GitHubIcon";
 import ContactCard from "./ContactCard";
 
+const contactsList = [
+  {
+    icon: <EmailIcon />,
+    title: "info@d0m1.cz",
+    description: "Email",
+    link: "mailto:info@d0m1.cz",
+  },
+  {
+    icon: <GitHubIcon />,
+    title: "dominik060",
+    description: "GitHub",
+    link: "https://github.com/dominik060",
+  },
+];
+
 export default function ContactsCardBoard() {
   return (
     <div className="flex flex-wrap justify-evenly gap-4 px-8">
-      <ContactCard
-        Icon={<EmailIcon />}
-        title="info@d0m1.cz"
-        description="Email"
-        link="mailto:info@d0m1.cz"
-      />
-      <ContactCard
-        Icon={<GitHubIcon />}
-        title="dominik060"
-        description="GitHub"
-        link="https://github.com/dominik060"
-      />
+      {contactsList &&
+        contactsList.map((contact) => (
+          <ContactCard
+            Icon={contact.icon}
+            title={contact.title}
+            description={contact.description}
+            link={contact.link}
+          />
+        ))}
     </div>
   );
 }
