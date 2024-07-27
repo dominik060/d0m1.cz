c108a3993a7641e9ce5cf31ea8e23b95626811ad
Dominik Bartuška <dominikbartuska@gmail.com>
2024-07-27 15:56:16 +0200

    add className prop

--- src/components/aboutme/AboutMeTitle.tsx
+++ src/components/aboutme/AboutMeTitle.tsx
@@ -1,11 +1,13 @@
-export default function AboutMeTitle() {
+export default function AboutMeTitle({ className }: { className?: string }) {
   return (
-    <h2 className="text-2xl md:text-4xl text-center">
-      Hello, I am{" "}
-      <span className="inline-block bg-gradient-apple bg-clip-text font-bold text-transparent underline duration-300 hover:-rotate-6">
-        Dominik Bartuška
-      </span>
-      !
-    </h2>
+    <div className={className}>
+      <h2 className="text-2xl md:text-4xl text-center">
+        Hello, I am{" "}
+        <span className="inline-block bg-gradient-apple bg-clip-text font-bold text-transparent underline duration-300 hover:-rotate-6">
+          Dominik Bartuška
+        </span>
+        !
+      </h2>
+    </div>
   );
 }
