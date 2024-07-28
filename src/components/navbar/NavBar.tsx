6ef425a5df4c12c84abb035e493011744f868907
Dominik Bartuška <dominikbartuska@gmail.com>
2024-07-28 13:24:47 +0200

    make navbar mobile responsive

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -10,11 +10,14 @@ type Props = {
 
 export default function NavBar({ className, ...props }: Props) {
   return (
-    <nav {...props} className="flex items-center justify-between px-16 py-8">
+    <nav
+      {...props}
+      className="flex items-center justify-between px-8 py-4 sm:px-16 sm:py-8"
+    >
       <Link href="/" title="Move to home page" className="">
         <Logo className="w-16" aria-hidden="true" />
       </Link>
-      <div className="flex flex-wrap items-center justify-end gap-8">
+      <div className="flex flex-wrap items-center justify-end gap-6 sm:gap-8">
         <NavLinks
           className="flex flex-nowrap gap-8 text-h4"
           navLinkList={[{ name: "Contact", href: "#contact" }]}
