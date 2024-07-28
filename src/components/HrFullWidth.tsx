86aad81e7955ac8f6a7fd0ca4d34b9b258e1b883
Dominik Bartuška <dominikbartuska@gmail.com>
2024-07-28 12:56:02 +0200

    remove unused prop

--- src/components/HrFullWidth.tsx
+++ src/components/HrFullWidth.tsx
@@ -1,8 +1,4 @@
-type Props = {
-  className?: string;
-};
-
-export default function HrFullWidth({ className }: Props) {
+export default function HrFullWidth() {
   return (
     <hr className="h-[2px] w-screen transform border-0 bg-current hr-line:-translate-x-[calc((100vw-80rem)/2)]" />
   );
