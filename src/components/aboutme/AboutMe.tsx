6a1053992e01ff4a540cbe0873d5e94b98f15de6
Dominik Bartuška <dominikbartuska@gmail.com>
2024-07-28 12:54:51 +0200

    remove unused prop

--- src/components/aboutme/AboutMe.tsx
+++ src/components/aboutme/AboutMe.tsx
@@ -1,11 +1,7 @@
 import AboutMeTitle from "./AboutMeTitle";
 import ContactsCardBoard from "./ContactsCardBoard";
 
-type Props = {
-  className?: string;
-};
-
-export default function AboutMe({ className }: Props) {
+export default function AboutMe() {
   return (
     <div className="my-16 flex flex-col justify-center sm:my-20 md:my-24">
       <AboutMeTitle className="mb-12 sm:mb-16 md:mb-20" />
