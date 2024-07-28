e3a31266ad797a22d3bd0e480450f4e319ec8aef
Dominik Bartuška <dominikbartuska@gmail.com>
2024-07-28 12:45:42 +0200

    improve text sizing

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,7 @@
 export default function Hero() {
   return (
     <div className="flex h-[80%] flex-col items-center justify-center text-center">
-      <h1 className="text-4xl md:text-6xl bg-gradient-hero-light bg-clip-text font-black text-transparent dark:bg-gradient-hero-dark">
+      <h1 className="bg-gradient-hero-light bg-clip-text text-3xl font-black text-transparent dark:bg-gradient-hero-dark sm:text-5xl md:text-6xl">
         {/* text-h1 sm:text-h1 md:text-[60px] lg:text-hero */}
         Forming Tomorrow's
         <br />
