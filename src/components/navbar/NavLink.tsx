6bef7790103b7d5dec1d5688380b424d666004cd
Dominik Bartuška <dominikbartuska@gmail.com>
2024-07-28 13:14:02 +0200

    reformat

--- src/components/navbar/NavLink.tsx
+++ src/components/navbar/NavLink.tsx
@@ -1,23 +1,23 @@
 import Link from "next/link";
 
 interface navLinkItem {
-    name: string;
-    href: string;
+  name: string;
+  href: string;
 }
 type Props = {
-    navLinkList?: navLinkItem[];
-    className?: string;
+  navLinkList?: navLinkItem[];
+  className?: string;
 };
 
 export default function NavLinks({ navLinkList, className }: Props) {
-    if (!navLinkList) return;
-    return (
-        <div className={className}>
-            {navLinkList.map(({ name, href }) => (
-                <Link key={href} href={href}>
-                    {name}
-                </Link>
-            ))}
-        </div>
-    );
+  if (!navLinkList) return;
+  return (
+    <div className={className}>
+      {navLinkList.map(({ name, href }) => (
+        <Link key={href} href={href}>
+          {name}
+        </Link>
+      ))}
+    </div>
+  );
 }
