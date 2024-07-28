1c117c5ebde6882937b6c18c7edad136e48f5048
Dominik Bartuška <dominikbartuska@gmail.com>
2024-07-28 13:08:34 +0200

    add id

--- src/components/aboutme/AboutMe.tsx
+++ src/components/aboutme/AboutMe.tsx
@@ -3,7 +3,10 @@ import ContactsCardBoard from "./ContactsCardBoard";
 
 export default function AboutMe() {
   return (
-    <div className="my-16 flex flex-col justify-center sm:my-20 md:my-24">
+    <div
+      className="my-16 flex flex-col justify-center sm:my-20 md:my-24"
+      id="contact"
+    >
       <AboutMeTitle className="mb-12 sm:mb-16 md:mb-20" />
       <ContactsCardBoard />
     </div>
