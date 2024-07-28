1e7acb1c4e4aa37bd25bb89c05faea4a2a3a75ad
Dominik Bartuška <dominikbartuska@gmail.com>
2024-07-28 18:02:36 +0200

    add more metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,8 +7,34 @@ import type { Viewport } from "next";
 
 export const metadata: Metadata = {
   title: "Dominik Bartuška",
-  description: "Personal web of Dominik Bartuška",
+  description: "Personal website of Dominik Bartuška",
   creator: "Dominik Bartuška",
+  publisher: "Dominik Bartuška",
+  authors: [{ name: "Dominik Bartuška", url: "https://d0m1.cz" }],
+  generator: "Next.js",
+  twitter: {
+    card: "summary",
+    site: "https://d0m1.cz",
+    creator: "@DominikBartuska",
+    description: "Personal website of Dominik Bartuška",
+    title: "d0m1.cz",
+  },
+  keywords: [
+    "dominik bartuska",
+    "Dominik Bartuška",
+    "code",
+    "web development",
+    "typescript",
+    "next.js",
+    "web dev",
+    "html",
+    "css",
+    "python",
+    "gym",
+    "muscle",
+  ],
+  applicationName: "Dominik Bartuška's Website",
+  referrer: "origin-when-cross-origin",
   icons: [
     {
       rel: "icon",
