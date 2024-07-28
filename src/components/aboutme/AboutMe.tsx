586dd5c84a39f40e3b519572f309e1dbd331ded9
Dominik Bartuška <dominikbartuska@gmail.com>
2024-07-28 12:54:31 +0200

    adjust y axis margins for different device widths

--- src/components/aboutme/AboutMe.tsx
+++ src/components/aboutme/AboutMe.tsx
@@ -7,8 +7,8 @@ type Props = {
 
 export default function AboutMe({ className }: Props) {
   return (
-    <div className="my-24 flex flex-col justify-center">
-      <AboutMeTitle className="mb-8" />
+    <div className="my-16 flex flex-col justify-center sm:my-20 md:my-24">
+      <AboutMeTitle className="mb-12 sm:mb-16 md:mb-20" />
       <ContactsCardBoard />
     </div>
   );
