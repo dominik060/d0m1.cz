4bf8d4c61ed87d79296ab3119b63b9102aceebac
Dominik Bartuška <dominikbartuska@gmail.com>
2024-07-30 12:58:32 +0200

    move all favicons to root directory and update their used paths

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -11,17 +11,17 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: "#FAFAFA",
     icons: [
       {
-        src: "favicons/android-chrome-192x192.png",
+        src: "/android-chrome-192x192.png",
         sizes: "192x192",
         type: "image/png",
       },
       {
-        src: "favicons/android-chrome-512x512.png",
+        src: "/android-chrome-512x512.png",
         sizes: "512x512",
         type: "image/png",
       },
       {
-        src: "favicons/favicon.ico",
+        src: "/favicon.ico",
         sizes: "48x48",
         type: "image/x-icon",
       },
