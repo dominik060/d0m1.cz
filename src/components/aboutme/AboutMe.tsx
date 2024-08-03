b1a5d3cac140f5fca039fce888466c8fdcdef505
Dominik Bartuška <dominikbartuska@gmail.com>
2024-08-03 18:03:58 +0200

    decrease bottom margin for title

--- src/components/aboutme/AboutMe.tsx
+++ src/components/aboutme/AboutMe.tsx
@@ -5,7 +5,7 @@ import ContactsCardBoard from "./ContactsCardBoard";
 export default function AboutMe() {
   return (
     <div className="my-16 flex flex-col justify-center sm:my-20 md:my-24">
-      <AboutMeTitle className="mb-12 sm:mb-16 md:mb-20" />
+      <AboutMeTitle className="mb-6 sm:mb-8 md:mb-10" />
       <AboutMeParagraph />
       <ContactsCardBoard />
     </div>
