038d769a84369cbf49d4a82ddfa2173d41e3cd05
Dominik Bartuška <dominikbartuska@gmail.com>
2024-08-04 13:00:22 +0200

    update about me text

--- src/components/aboutme/AboutMeParagraph.tsx
+++ src/components/aboutme/AboutMeParagraph.tsx
@@ -5,11 +5,19 @@ export default function AboutMeParagraph({
 }) {
   return (
     <div className={className}>
-      <p className="text-center text-base">
-        I'm student from Czechia, I make websites, develop android apps, fly fpv
-        drones and do some photography.
+      <p className="text-center text-base md:text-xl">
+        I'm a student from Czechia with a love for all things tech and active! I
+        enjoy creating websites, developing Android apps, and flying FPV drones.
+        Photography is another passion of mine, capturing moments that tell a
+        story. When I'm not behind a screen, you'll find me staying active with
+        sports and hitting the gym. Fitness is a big part of my life, keeping me
+        energized and focused.
         <br />
-        You can contact me via email and look at some of my work on GitHub.
+        Feel free to reach out via email, and check out some of my projects on
+        GitHub. I'm always excited to connect and collaborate with like-minded
+        individuals.
+        <br />
+        Take care, Dominik Bartuška
       </p>
     </div>
   );
