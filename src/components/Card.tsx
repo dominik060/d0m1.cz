09ee75366d122fe0df19c0d8606b4120d0ea44bc
Dominik <dominikbartuska@gmail.com>
2024-08-04 21:42:41 +0200

    use tailwind merge

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,3 +1,7 @@
+import { twMerge } from "tailwind-merge";
+
+// → 'hover:bg-dark-red p-3 bg-[#B91C1C]'
+
 type Props = {
   className?: string;
   children: React.ReactNode;
@@ -6,10 +10,10 @@ export default function Card({ className, children }: Props) {
   className = " " + className;
   return (
     <div
-      className={
-        "rounded-lg border-2 border-black border-opacity-60 p-4 dark:border-white dark:border-opacity-60" +
+      className={twMerge(
+        "rounded-lg border-2 border-black border-opacity-60 p-4 dark:border-white dark:border-opacity-60",
         className
-      }
+      )}
     >
       {children}
     </div>
