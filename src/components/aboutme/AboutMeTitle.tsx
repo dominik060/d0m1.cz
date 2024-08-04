b1ac88751fa4d6b9b500347e7ee5f7c9bf4de005
Dominik Bartuška <dominikbartuska@gmail.com>
2024-08-04 12:59:55 +0200

    fix "!" wrapping

--- src/components/aboutme/AboutMeTitle.tsx
+++ src/components/aboutme/AboutMeTitle.tsx
@@ -3,10 +3,12 @@ export default function AboutMeTitle({ className }: { className?: string }) {
     <div className={className}>
       <h2 className="text-center text-2xl sm:text-3xl md:text-4xl">
         Hello, I am{" "}
-        <span className="inline-block bg-gradient-hero-light bg-clip-text font-bold text-transparent underline duration-300 hover:-rotate-6 dark:bg-gradient-hero-dark">
-          Dominik Bartuška
+        <span className="text-nowrap">
+          <span className="inline-block bg-gradient-hero-light bg-clip-text font-bold text-transparent underline duration-300 hover:-rotate-6 dark:bg-gradient-hero-dark">
+            Dominik Bartuška
+          </span>
+          !
         </span>
-        !
       </h2>
     </div>
   );
