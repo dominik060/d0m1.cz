98efc01d9046def079296e0c7137abb1cf7108e9
Dominik Bartuška <dominikbartuska@gmail.com>
2024-08-05 17:19:30 +0200

    change text alignment to left

--- src/components/aboutme/AboutMe.tsx
+++ src/components/aboutme/AboutMe.tsx
@@ -6,7 +6,7 @@ export default function AboutMe() {
   return (
     <div className="my-16 flex flex-col justify-center sm:my-20 md:my-24">
       <AboutMeTitle className="mb-6 sm:mb-8 md:mb-10" />
-      <AboutMeParagraph className="mb-12 sm:mb-16 md:mb-20" />
+      <AboutMeParagraph className="mb-12 flex justify-center sm:mb-16 md:mb-20" />
       <ContactsCardBoard />
     </div>
   );
