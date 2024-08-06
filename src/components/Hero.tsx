7f0590b6e3b4cb1149d4c4ce1e91a36591285eef
Dominik Bartuška <dominikbartuska@gmail.com>
2024-08-06 21:44:43 +0200

    setup after

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,7 @@
 export default function Hero() {
   return (
     <div className="flex h-[80%] flex-col items-center justify-center text-center">
-      <h1 className="bg-gradient-blue-light bg-clip-text text-3xl font-black text-transparent dark:bg-gradient-blue-dark sm:text-5xl md:text-6xl">
+      <h1 className="relative bg-gradient-blue-light bg-clip-text text-3xl font-black text-transparent after:absolute after:left-0 after:top-0 after:z-0 after:h-full after:w-full after:overflow-visible after:bg-red-700 after:opacity-0 after:duration-500 hover:after:opacity-100 dark:bg-gradient-blue-dark sm:text-5xl md:text-6xl">
         Forming Tomorrow's
         <br />
         Digital Landscape.
