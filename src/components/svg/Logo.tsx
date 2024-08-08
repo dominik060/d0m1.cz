f14de4647ae2fd1e72f6aaa93f9f117d042e4d00
Dominik Bartuška <dominikbartuska@gmail.com>
2024-08-08 14:00:03 +0200

    delete unused style

--- src/components/svg/Logo.tsx
+++ src/components/svg/Logo.tsx
@@ -21,46 +21,21 @@ export default function Logo({ className, props }: Props) {
       <g id="layer1" className="w-16 fill-current">
         <path
           id="path11"
-          // style="fill:#09090b;fill-opacity:1;stroke-width:0.461937"
           d="M 70,0 V 29.999717 H 99.999726 A 30,30 0 0 0 70,0 Z"
         />
-        <rect
-          // style="fill:#09090b;fill-opacity:1;stroke-width:0.694679"
-          id="rect11"
-          width="30"
-          height="30"
-          x="35"
-          y="0"
-        />
+        <rect id="rect11" width="30" height="30" x="35" y="0" />
         <path
           id="path1"
-          // style="fill:#09090b;fill-opacity:1;stroke-width:0.461937"
           d="M 29.999726,0 V 29.999717 H 0 A 30,30 0 0 1 29.999726,0 Z"
         />
-        <rect
-          // style="fill:#09090b;fill-opacity:1;stroke-width:0.694679"
-          id="rect1"
-          width="30"
-          height="30"
-          x="35"
-          y="70"
-        />
-        <rect
-          // style="fill:#09090b;fill-opacity:1;stroke-width:0.694679"
-          id="rect2"
-          width="30"
-          height="30"
-          x="35"
-          y="35"
-        />
+        <rect id="rect1" width="30" height="30" x="35" y="70" />
+        <rect id="rect2" width="30" height="30" x="35" y="35" />
         <path
           id="path2"
-          // style="fill:#09090b;fill-opacity:1;stroke-width:0.461937"
           d="M 70,64.999717 V 35 H 99.999726 A 30,30 0 0 1 70,64.999717 Z"
         />
         <path
           id="path3"
-          // style="fill:#09090b;fill-opacity:1;stroke-width:0.461937"
           d="M 29.999726,64.999717 V 35 H 0 a 30,30 0 0 0 29.999726,29.999717 z"
         />
       </g>
