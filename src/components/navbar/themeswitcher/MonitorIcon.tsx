b2f49faf6b1f86f3028be0a893100cbc3e8705db
Dominik Bartuška <dominikbartuska@gmail.com>
2024-08-08 14:00:33 +0200

    rename properties

--- src/components/navbar/themeswitcher/MonitorIcon.tsx
+++ src/components/navbar/themeswitcher/MonitorIcon.tsx
@@ -15,9 +15,9 @@ export default function MonitorIcon({ className, active, onClick }: Props) {
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
       className={active ? className + " text-white dark:text-black" : className}
       onClick={onClick}
     >
