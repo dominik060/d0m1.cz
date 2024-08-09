339570bfbee60ded386317331231187c59d32808
Dominik Bartuška <dominikbartuska@gmail.com>
2024-08-09 10:26:00 +0200

    remove unused code

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -13,19 +13,13 @@ export default function Hero() {
   useEffect(() => {
     if (!mouse || !ref.current || !mouse.x || !mouse.y) return;
 
-    console.log(mouse);
-
     const heroText = document.getElementById("hero-text")!;
-
     const bubble = document.getElementById("bubble")!;
 
     heroText.onmousemove = (e) => {
       const rect = heroText.getBoundingClientRect(),
         x = mouse.x!,
         y = mouse.y!;
-
-      heroText.style.setProperty("--mouse-x", `${x}px`);
-      heroText.style.setProperty("--mouse-y", `${y}px`);
 
       if (0 > x || x > rect.width || 0 > y || y > rect.height) return;
 
@@ -38,7 +32,6 @@ export default function Hero() {
     <div className="relative z-10 flex h-[80%] flex-col items-center justify-center text-center">
       <h1
         className="group/hero relative z-30 bg-gradient-blue-light bg-clip-text text-3xl font-black text-transparent dark:bg-gradient-blue-dark sm:text-5xl md:text-6xl"
-        // after:absolute after:left-0 after:top-0 after:z-30 after:h-full after:w-full after:overflow-visible after:bg-after-dark after:opacity-0 after:duration-500 hover:after:opacity-100
         id="hero-text"
         ref={ref}
       >
