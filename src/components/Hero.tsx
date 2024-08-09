a9f455866a881aaa03b72dc99779338f0b9a099f
Dominik Bartuška <dominikbartuska@gmail.com>
2024-08-09 10:29:50 +0200

    increase effect animation duration

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -43,7 +43,7 @@ export default function Hero() {
         <br />
         Digital Landscape.
         <div
-          className="bg-radial-dark absolute left-0 top-0 h-[800px] w-[800px] opacity-0 duration-100"
+          className="bg-radial-dark absolute left-0 top-0 h-[800px] w-[800px] opacity-0 duration-200"
           id="bubble"
         ></div>
       </h1>
