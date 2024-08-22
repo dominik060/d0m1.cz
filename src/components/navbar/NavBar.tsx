623872c0c44d3b89f10d1ceb7a8f3ddb659adbb0
Dominik Bartuška <dominikbartuska@gmail.com>
2024-08-22 21:52:49 +0200

    remove hover lower opacity on logo

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -8,9 +8,9 @@ import { NavigationMenu } from "@/components/ui/navigation-menu";
 export default function NavBar() {
   return (
     <NavigationMenu className="flex max-w-full items-center justify-between py-4 sm:py-8">
-      <Link href="/" title="Move to home page" className="">
+      <Link href="/" title="Go to home page" className="">
         <Button variant="ghost" className="m-0 h-full w-full">
-          <Logo className="w-10 hover:opacity-80 sm:w-14" aria-hidden="true" />
+          <Logo className="w-10 sm:w-14" aria-hidden="true" />
         </Button>
       </Link>
       <div className="flex flex-wrap items-center justify-end gap-6 sm:gap-8">
