a29feb625c43f8e57017579c3c7faac896b013a3
Dominik Bartuška <dominikbartuska@gmail.com>
2024-08-22 21:18:40 +0200

    add hover effect

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -12,11 +12,11 @@ export default function NavBar({ className, ...props }: Props) {
   return (
     <nav {...props} className="flex items-center justify-between py-4 sm:py-8">
       <Link href="/" title="Move to home page" className="">
-        <Logo className="w-11 sm:w-16" aria-hidden="true" />
+        <Logo className="w-11 hover:opacity-80 sm:w-16" aria-hidden="true" />
       </Link>
       <div className="flex flex-wrap items-center justify-end gap-6 sm:gap-8">
         <NavLinks
-          className="flex flex-nowrap gap-8 text-xl"
+          className="flex flex-nowrap gap-8 text-xl duration-500 hover:underline hover:opacity-80"
           navLinkList={[{ name: "About Me", href: "#aboutme" }]}
         />
         <ThemeSwitcher />
