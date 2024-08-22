6ba5841cbf7c0b9c090e65a14c1f6e53b746c39d
Dominik Bartuška <dominikbartuska@gmail.com>
2024-08-22 21:25:52 +0200

    add shadcn

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,7 +14,13 @@ const config: Config = {
       xl: { min: "1660px" },
       "hr-line": "80rem",
     },
-
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
       colors: {
         black: "#09090B",
@@ -50,13 +56,23 @@ const config: Config = {
           "60%": { transform: "rotate(0deg)" },
           "100%": { transform: "rotate(0deg)" },
         },
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
       },
       animation: {
         "waving-tail": "wave 2s linear",
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
   darkMode: "class",
 };
 export default config;
