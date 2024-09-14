fbd534401448fe75404eb7907ded7046508eed12
Dominik Bartuška <dominikbartuska@gmail.com>
2024-09-14 12:33:36 +0200

    expand width

--- src/components/aboutme/Footer.tsx
+++ src/components/aboutme/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="flex h-24 flex-col items-center justify-center">
+    <footer className="w-[calc(100% + 2rem)] -mx-8 flex h-24 flex-col items-center justify-center sm:-mx-16">
       <p className="text-sm">&copy; {new Date().getFullYear()} Dominik</p>
     </footer>
   );
