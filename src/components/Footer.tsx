0a79853085183865d7c6443330b0b1396998a2ce
Dominik Bartuška <dominikbartuska@gmail.com>
2024-09-14 13:03:14 +0200

    add canvas cursor toggle

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,23 @@
+"use client";
+
+import { useState } from "react";
 import CanvasCursor from "./CanvasCursor";
+import { Button } from "./ui/button";
 
 export default function Footer() {
+  const [showCanvasCursor, setShowCanvasCursor] = useState(false);
+
+  function toggleCanvasCursor() {
+    document.body.classList.toggle("overflow-hidden");
+    setShowCanvasCursor(!showCanvasCursor);
+  }
+
   return (
     <footer className="w-[calc(100% + 2rem)] relative -mx-8 flex h-24 flex-col items-center justify-center sm:-mx-16">
-      <p className="text-sm">
+      <Button className="text-sm" variant="ghost" onClick={toggleCanvasCursor}>
         &copy; {new Date().getFullYear()} Dominik Bartuška
-      </p>
-      <CanvasCursor />
+      </Button>
+      {showCanvasCursor && <CanvasCursor />}
     </footer>
   );
 }
