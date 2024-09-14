e94a9c7492557da03d0ac0ff9eab5027befe2b21
Dominik Bartuška <dominikbartuska@gmail.com>
2024-09-14 12:55:06 +0200

    use CanvasCursor

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,12 @@
+import CanvasCursor from "./CanvasCursor";
+
 export default function Footer() {
   return (
-    <footer className="w-[calc(100% + 2rem)] -mx-8 flex h-24 flex-col items-center justify-center sm:-mx-16">
+    <footer className="w-[calc(100% + 2rem)] relative -mx-8 flex h-24 flex-col items-center justify-center sm:-mx-16">
       <p className="text-sm">
         &copy; {new Date().getFullYear()} Dominik Bartuška
       </p>
+      <CanvasCursor />
     </footer>
   );
 }
