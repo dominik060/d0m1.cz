4fa1fcc802acb5d2f16f399d7d8d74f42c1b0b11
Dominik Bartuška <dominikbartuska@gmail.com>
2024-09-14 12:32:54 +0200

    add height

--- src/components/aboutme/Footer.tsx
+++ src/components/aboutme/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="flex flex-col items-center justify-center">
+    <footer className="flex h-24 flex-col items-center justify-center">
       <p className="text-sm">&copy; {new Date().getFullYear()} Dominik</p>
     </footer>
   );
