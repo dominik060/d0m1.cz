8f1839db4902fc67a98dcb00da4b7e93062e9de8
Dominik Bartuška <dominikbartuska@gmail.com>
2024-09-14 12:55:00 +0200

    create canvasCursor

---
+++ src/components/CanvasCursor.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import useCanvasCursor from "@/utils/canvasCursor";
+
+const CanvasCursor = () => {
+  useCanvasCursor();
+
+  return (
+    <canvas className="pointer-events-none fixed inset-0 z-[999]" id="canvas" />
+  );
+};
+export default CanvasCursor;
