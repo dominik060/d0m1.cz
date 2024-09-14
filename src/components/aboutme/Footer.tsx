ade6c7fbdfa42110ce8d78ce1484e6b34d6e378d
Dominik Bartuška <dominikbartuska@gmail.com>
2024-09-14 12:34:03 +0200

    add surname

--- src/components/aboutme/Footer.tsx
+++ src/components/aboutme/Footer.tsx
@@ -1,7 +1,9 @@
 export default function Footer() {
   return (
     <footer className="w-[calc(100% + 2rem)] -mx-8 flex h-24 flex-col items-center justify-center sm:-mx-16">
-      <p className="text-sm">&copy; {new Date().getFullYear()} Dominik</p>
+      <p className="text-sm">
+        &copy; {new Date().getFullYear()} Dominik Bartuška
+      </p>
     </footer>
   );
 }
