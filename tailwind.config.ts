2495474fec9b83cdc486a3a6fb1e7124f9dda9a4
Dominik <dominikbartuska@gmail.com>
2024-09-25 19:07:44 +0200

    add appear animation

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -64,11 +64,20 @@ const config: Config = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
+        appear: {
+          "0%": {
+            opacity: "0",
+          },
+          "100%": {
+            opacity: "1",
+          },
+        },
       },
       animation: {
         "waving-tail": "wave 2s linear",
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        appear: "appear 0.5s ease-in-out",
       },
     },
   },
