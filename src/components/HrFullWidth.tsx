834aebd8201d13baeb0eaedaab0d70677b83fcca
Dominik <dominikbartuska@gmail.com>
2024-09-25 19:13:22 +0200

    add classname prop

--- src/components/HrFullWidth.tsx
+++ src/components/HrFullWidth.tsx
@@ -1,5 +1,12 @@
-export default function HrFullWidth() {
+import { twMerge } from "tailwind-merge";
+
+export default function HrFullWidth({ className }: { className?: string }) {
   return (
-    <hr className="h-[2px] w-screen -translate-x-[calc(2rem)] transform border-0 bg-current sm:-translate-x-[calc(4rem)] hr-line:-translate-x-[calc((100vw-80rem+8rem)/2)]" />
+    <hr
+      className={twMerge(
+        "h-[2px] w-screen -translate-x-[calc(2rem)] transform border-0 bg-current sm:-translate-x-[calc(4rem)] hr-line:-translate-x-[calc((100vw-80rem+8rem)/2)]",
+        className
+      )}
+    />
   );
 }
